83b6b20669e3ebe6c616593c9fa2059b62a53199
gayanthawanni <gayanthawannisekara@gmail.com>
2026-04-16 10:31:36 +0530

    add WebSocket config and update OAuth security

--- backend/src/main/java/backend/config/WebSocketConfig.java
+++ backend/src/main/java/backend/config/WebSocketConfig.java
@@ -18,6 +18,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:3000", "http://localhost:3001")
+                .withSockJS();
     }
 }
